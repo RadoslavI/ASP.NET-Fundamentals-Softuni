79f627d88f2ca1cbb9051019d6cb6b33a9b04bf5
Radoslav Ivanov <radogi001@gmail.com>
2022-09-30 23:10:48 +0300

    fixed merge problem

---
+++ 01.ASP-Introduction/WebShopDemo.Core/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebShopDemo.Core.Data.Models;
+
+namespace WebShopDemo.Core.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ 01.ASP-Introduction/WebShopDemo.Core/Services/ProductService.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using WebShopDemo.Core.Contracts;
+using WebShopDemo.Core.Data.Common;
+using WebShopDemo.Core.Data.Models;
+using WebShopDemo.Core.Models;
+
+namespace WebShopDemo.Core.Services
+{
+    /// <summary>
+    /// Manipulates product data
+    /// </summary>
+    public class ProductService : IProductService
+    {
+        private readonly IConfiguration config;
+
+        private readonly IRepository repo;
+
+        /// <summary>
+        /// IoC 
+        /// </summary>
+        /// <param name="config">Application configuration</param>
+        public ProductService(
+            IConfiguration _config,
+            IRepository _repo)
+        {
+            this.config = _config;
+        }
+
+        /// <summary>
+        /// Add new product
+        /// </summary>
+        /// <param name="productDto">Product model</param>
+        /// <returns></returns>
+        public async Task Add(ProductDto productDto)
+        {
+            var product = new Product()
+            {
+                Name = productDto.Name,
+                Price = productDto.Price,
+                Quantity = productDto.Quantity
+            };
+
+            await repo.AddAsync(product);
+            await repo.SaveChangesAsync();
+        }
+
+        /// <summary>
+        /// Gets all products
+        /// </summary>
+        /// <returns>List of products</returns>
+        public async Task<IEnumerable<ProductDto>> GetAll()
+        {
+            string dataPath = config.GetSection("DataFiles:Products").Value;
+            string data = await File.ReadAllTextAsync(dataPath);
+
+            return JsonConvert.DeserializeObject<IEnumerable<ProductDto>>(data);
+        }
+    }
+}
