8c236b9ecdee31cb3e30ade490e24a91f9982f93
Radoslav Ivanov <radogi001@gmail.com>
2022-10-06 23:51:51 +0300

    added the create operation

--- 03.Workshop-ForumApp/ForumAppStamo/Controllers/PostController.cs
+++ 03.Workshop-ForumApp/ForumAppStamo/Controllers/PostController.cs
@@ -28,5 +28,49 @@ namespace ForumAppStamo.Controllers
 
             return View(model);
         }
+
+        public IActionResult Add()
+        {  
+            var model = new PostViewModel();
+
+            ViewData["Title"] = "Add new Post";
+
+            return View("Edit", model);
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> Edit(PostViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewData["Title"] = model.Id == 0 ? "Add new Post" : "Edit Post";
+
+                return View(model);
+            }
+
+            if (model.Id == 0)
+            {
+                context.Posts.Add(new Post()
+                {
+                    Title = model.Title,
+                    Content = model.Content
+                });
+            }
+            else
+            {
+                var post = await context.Posts.FindAsync(model.Id);
+
+                if (post != null)
+                {
+                    post.Title = model.Title;
+                    post.Content = model.Content;
+                }
+            }
+
+            await context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
