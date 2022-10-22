f679c8d6afffb5e7d0f10eb56685822aa469231e
Radoslav Ivanov <radogi001@gmail.com>
2022-10-23 00:21:42 +0300

    finished the exam

---
+++ 07.Exam/Library/Data/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Policy;
+
+namespace Library.Data.Models
+{
+    public class Category
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        public ICollection<Book> Books { get; set; } 
+            = new List<Book>();
+    }
+}
